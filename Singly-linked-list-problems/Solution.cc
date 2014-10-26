5858c789955ac539a776b47732693141b1ebe665
quxia83 <quxiapisces@gmail.com>
2014-10-26 13:01:31 -0400

    Update Solution.cc
    fixed a bug in hasCircle()

--- Singly-linked-list-problems/Solution.cc
+++ Singly-linked-list-problems/Solution.cc
@@ -144,12 +144,11 @@ public:
 		ListNode* slow = head;
 		ListNode* fast = head;
 		while(fast&&fast->next){
+			slow = slow->next;
+			fast = fast->next->next;
 			if(slow == fast){
 				return true;
 			}
-			
-			slow = slow->next;
-			fast = fast->next->next;
 		}
 		
 		return false;
